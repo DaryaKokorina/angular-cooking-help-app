5a3bf400e4d1f7cc07c22693ccc2e15451359aa5
Siarhei Kasko <siarhei.kasko@instinctools.com>
2020-11-26 16:45:28 +0300

    feat: set up main login

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'A test Recipe', 
+      'This is simpy a test recipe',
+      'https://hips.hearstapps.com/hmg-prod/images/chicken-fajitas-horizontal-jpg-1522721531.jpg'),
+      new Recipe(
+        'A test Recipe', 
+        'This is simpy a test recipe',
+        'https://hips.hearstapps.com/hmg-prod/images/chicken-fajitas-horizontal-jpg-1522721531.jpg'),
+  ];
 
   constructor() { }
 
