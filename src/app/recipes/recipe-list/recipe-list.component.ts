916cc55741b786363a1b85e5ae8766bfa01fe88c
Siarhei Kasko <siarhei.kasko@instinctools.com>
2020-12-08 18:20:25 +0300

    feat: provide recipe-details component with data

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,13 +7,15 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() selectedRecipe: EventEmitter<Recipe> = new EventEmitter<Recipe>();
+
   recipes: Recipe[] = [
     new Recipe(
       'A test Recipe', 
       'This is simpy a test recipe',
       'https://hips.hearstapps.com/hmg-prod/images/chicken-fajitas-horizontal-jpg-1522721531.jpg'),
       new Recipe(
-        'A test Recipe', 
+        'A test Recipe 1', 
         'This is simpy a test recipe',
         'https://hips.hearstapps.com/hmg-prod/images/chicken-fajitas-horizontal-jpg-1522721531.jpg'),
   ];
@@ -21,6 +23,10 @@ export class RecipeListComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onRecipeSelectHandler(recipe: Recipe): void {
+    this.selectedRecipe.emit(recipe);
   }
 
 }
