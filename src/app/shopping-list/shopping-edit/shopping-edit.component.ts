54a4cafdc2fdb1d69f4deefd0572596b8d6445ac
Siarhei Kasko <siarhei.kasko@instinctools.com>
2020-12-08 22:23:10 +0300

    feat: add posibility for eddin goog to shopping-list

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
+import { Ingredient } from '../../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit {
+  @Output() newIngredient: EventEmitter<Ingredient>
+    = new EventEmitter<Ingredient>();
+
+  @ViewChild('nameInput') nameInputRef: ElementRef;
+  @ViewChild('amountInput') amountInputRef: ElementRef;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  addGood(): void {
+    const ingName = this.nameInputRef.nativeElement.value;
+    const ingAmount = Number(this.amountInputRef.nativeElement.value);
+    const newIngredient = new Ingredient(ingName, ingAmount);
+    this.newIngredient.emit(newIngredient);
+  }
 }
