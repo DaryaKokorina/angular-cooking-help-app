c4a67969223fe59365606df01eae1444a163c509
Siarhei Kasko <siarhei.kasko@instinctools.com>
2020-12-25 21:40:47 +0300

    feat: add routing for edit recipe component

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-recipe-edit',
   templateUrl: './recipe-edit.component.html',
   styleUrls: ['./recipe-edit.component.css']
 })
-export class RecipeEditComponent implements OnInit {
+export class RecipeEditComponent implements OnInit, OnDestroy{
+  id: number;
+  editMode = false;
 
-  constructor() { }
+  ngUnsusbcribe = new Subject();
+
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.route.params.pipe(
+      takeUntil(this.ngUnsusbcribe)
+    ).subscribe((params: Params) => {
+      this.id = +params['id'];
+      this.editMode = params['id'] != null;
+      console.log(this.editMode);
+    });
+  }
+
+  ngOnDestroy(){
+    this.ngUnsusbcribe.next(null);
+    this.ngUnsusbcribe.complete();
   }
 
 }
